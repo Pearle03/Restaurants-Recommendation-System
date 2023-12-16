a18dbd94e70364f87c8fb9e0a51dc69e81e4ae5b
n1ml03 <56970683+n1ml03@users.noreply.github.com>
2023-12-16 13:24:27 +0700

    commit

--- Static/StaticRecommendation.scala
+++ Static/StaticRecommendation.scala
@@ -1,138 +1,74 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.mllib.recommendation.{ALS, Rating}
-import java.text.SimpleDateFormat
+import scala.collection.JavaConverters._
 
 val spark = SparkSession.builder()
   .appName("Yelp Recommendation System")
   .getOrCreate()
+import spark.implicits._
 
-val type_of_restaurant = readLine("Enter type of Food:").toLowerCase
+// User inputs
+val typeOfRestaurant = readLine("Enter type of Food:").toLowerCase
 val city = readLine("Enter City:").toLowerCase
-var residingState = readLine("Enter State:").toLowerCase
-val user_id = readLine("Enter User ID:")
+val residingState = readLine("Enter State:").toLowerCase
+val userId = readLine("Enter User ID:")
 
+// Paths
 val businessDataPath = "yelp_academic_dataset_business.csv"
 val reviewDataPath = "yelp_academic_dataset_review.csv"
 val outputPath = "/"
 
-residingState = residingState.toLowerCase
-
+// Read and filter business data
 val businessDF = spark.read
   .option("header", "true")
-  .option("parserLib", "UNIVOCITY")
   .csv(businessDataPath)
-  .select(lower(col("categories")).alias("categories"), col("business_id"), col("name"), col("stars"))
-  .filter(col("categories").contains("restaurant") &&
-    col("categories").contains(type_of_restaurant) &&
-    col("city") === city &&
-    col("state") === residingState)
+  .filter($"categories".contains("restaurant") &&
+    lower($"categories").contains(typeOfRestaurant) &&
+    lower($"city") === city &&
+    lower($"state") === residingState)
+  .select($"business_id", $"name", $"stars")
 
-val restaurantList = businessDF
-  .rdd
-  .map(row => row.getString(1))
-  .collect()
-
-val ResNameRat = businessDF
-  .rdd
-  .map(row => (row.getString(2), row.getDouble(3)))
-  .collectAsMap()
-
+// Read and filter review data
 val reviewDF = spark.read
   .option("header", "true")
-  .option("parserLib", "UNIVOCITY")
   .csv(reviewDataPath)
-  .withColumn("dateReviewed", unix_timestamp(col("date"), "yyyy-MM-dd"))
-  .filter(col("business_id").isin(restaurantList: _*))
+  .filter($"business_id".isin(businessDF.select("business_id").as[String].collect(): _*))
 
-val userReviewed = reviewDF
-  .filter(col("user_id") === user_id)
-  .count() > 0
+// Check if user has reviewed
+val userReviewed = reviewDF.filter($"user_id" === userId).count() > 0
 
-val hp = reviewDF
+// Prepare data for ALS
+val ratingsRDD = reviewDF.as[(String, String, String, Float)]
+  .map{ case (user, _, business, rating) => Rating(user.hashCode, business.hashCode, rating) }
   .rdd
-  .map { row =>
-    val uid = row.getString(0).replace("[", "")
-    val review_bid = row.getString(3)
-    val rating = row.getFloat(5)
-    val dateReviewed = row.getLong(7)
-    (uid, review_bid, rating, dateReviewed)
-  }
-  .aggregateByKey((0.0, 0L))(
-    (acc, value) => (acc._1 + value._3, Math.max(acc._2, value._4)),
-    (acc1, acc2) => (acc1._1 + acc2._1, Math.max(acc1._2, acc2._2))
-  )
-  .map {
-    case (uid, review_bid, totalRating, maxDate) =>
-      val uid_bid = s"$uid|$review_bid"
-      val rating_date = s"${totalRating.toFloat}|$maxDate"
-      (uid_bid, rating_date)
-  }
-  .collectAsMap()
 
-val userIdToNum = new util.HashMap[String, Int]()
-val businessIdToNum = new util.HashMap[String, Int]()
-var usercount = 2
-
-for (i <- hp.keySet().toArray()) {
-  val tempArray = i.toString().split("\\|")
-  val tempuid = tempArray(0)
-  val tempbid = tempArray(1)
-
-  userIdToNum.put(tempuid, if (tempuid == user_id) 1 else usercount)
-  businessIdToNum.put(tempbid, usercount)
-  usercount += 1
-}
-
-val ALSList = hp.keySet().toArray().map { i =>
-  val tempArray = i.toString().split("\\|")
-  val temp = s"${userIdToNum.get(tempArray(0))}::${businessIdToNum.get(tempArray(1))}"
-  val valueArray = hp.get(i).toString().split("\\|")
-  s"$temp::${valueArray(0)}::${valueArray(1)}"
-}
-
-val testHM = businessIdToNum.keySet().toArray().map { i =>
-  s"1::${businessIdToNum.get(i)}::0"
-}
-
-sc.parallelize(ALSList, 1).saveAsTextFile(s"$outputPath/RatingData9.dat")
-sc.parallelize(testHM, 1).saveAsTextFile(s"$outputPath/TestRatingData9.dat")
+// ALS Model Training
+val rank = 5
+val numIterations = 10
+val model = ALS.train(ratingsRDD, rank, numIterations, 0.01)
 
 if (userReviewed) {
-  val data = sc.textFile(s"$outputPath/RatingData9.dat/part-00000")
-  val ratings = data.map(_.split("::") match {
-    case Array(userID, businessID, rating, timestamp) =>
-      Rating(userID.toInt, businessID.toInt, rating.toDouble)
-  })
-  val testData = sc.textFile(s"$outputPath/TestRatingData9.dat/part-00000")
-  val testing = testData.map(_.split("::") match {
-    case Array(userID, businessID, rating) =>
-      Rating(userID.toInt, businessID.toInt, rating.toDouble)
-  })
-  val rank = 5
-  val numIterations = 10
-  val model = ALS.train(ratings, rank, numIterations, 0.01)
-  val usersProducts = testing.map {
-    case Rating(user, product, ratings) =>
-      (user, product)
-  }
-  val predictions = model.predict(usersProducts).map {
-    case Rating(user, product, rate) =>
-      ((user, product), rate)
-  }
-  val l = predictions.sortBy(-_._2).take(5)
-  val Top5Rest = l.map { case ((_, businessID), rate) => businessIdToNum.inverse.get(businessID).toString -> rate }
-  println("Top 5 Restaurants:")
-  Top5Rest.foreach {
-    case (businessName, rate) =>
-      println(s"${ResNameRat(businessName)} $rate")
-  }
+  // Prediction for a user
+  val userProducts = businessDF.select($"business_id").distinct
+    .map(_.getString(0).hashCode)
+    .map((userId.hashCode, _))
+    .rdd
+
+  val predictions = model.predict(userProducts)
+    .map(r => (r.product, r.rating))
+    .join(businessDF.rdd.map(r => (r.getString(0).hashCode, r.getString(1))))
+    .map(_._2)
+    .sortBy(-_._2)
+    .take(5)
+
+  println("Top 5 Recommended Restaurants:")
+  predictions.foreach { case (name, rating) => println(s"$name -> Rating: $rating") }
 } else {
-  val ratMapSeq = ResNameRat.toSeq.sortBy(-_._2).take(5)
-  println("Top 5 Restaurants based on the search is ->")
-  ratMapSeq.foreach {
-    case (businessName, rate) =>
-      println(s"$businessName $rate")
-  }
+  // Top 5 Restaurants based on overall ratings
+  val topRestaurants = businessDF.sort($"stars".desc).take(5)
+  println("Top 5 Restaurants based on overall ratings:")
+  topRestaurants.foreach(r => println(s"${r.getString(1)} -> Rating: ${r.getDouble(2)}"))
 }
+
 spark.stop()
